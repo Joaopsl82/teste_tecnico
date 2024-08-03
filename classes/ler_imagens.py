3030e75129bf9806f2b3b277000400f50ad9fc6b
João Vitor de Souza da Costa <62108840+Joaopsl82@users.noreply.github.com>
2024-08-03 16:20:46 -0300

    Ajustes Finais

--- classes/ler_imagens.py
+++ classes/ler_imagens.py
@@ -4,36 +4,35 @@ from PIL import Image
 from datetime import datetime
 
 class ImageToTexto:
-    # Passo 3: Ler textos das imagens para montar o CSV
-    def read_text(self, image_path):
+    def ler_texto(self, image_path):
         pytesseract.pytesseract.tesseract_cmd = r'C:\\Program Files\\Tesseract-OCR\\tesseract.exe'
         img = Image.open(image_path)
         texto = pytesseract.image_to_string(img)
         return texto
     
-    def extract_invoice_and_date(self, text):
+    def extrair_faturas_datas(self, texto):
         fatura = re.compile(r'Invoice #(\d+)')
         data = re.compile(r'\b\d{4}-\d{2}-\d{2}\b')
-        faturas = fatura.findall(text)
-        datas = data.findall(text)
+        faturas = fatura.findall(texto)
+        datas = data.findall(texto)
         return faturas, datas
     
     def arquivos(self):
-        caminho_arquivo = 'Faturas'
+        caminho_pasta = 'Faturas'
         data = []
-        for nome_arquivo in os.listdir(caminho_arquivo):
-            caminho_arquivo = os.path.join(caminho_arquivo, nome_arquivo)
+        for caminho_nome in os.listdir(caminho_pasta):
+            caminho_arquivo = os.path.join(caminho_pasta, caminho_nome)
             if os.path.isfile(caminho_arquivo):
-                text = self.read_text(caminho_arquivo)
+                text = self.ler_texto(caminho_arquivo)
                 if text:
-                    faturas, datas = self.extract_invoice_and_date(text)
-                    for fatura, data in zip(faturas, datas): 
+                    invoices, dates = self.extrair_faturas_datas(text)
+                    for invoice, date in zip(invoices, dates): 
                         data.append({
-                            'Numero da fatura': fatura,
-                            'Data da fatura': data,
-                            'URL da fatura': f'https://rpachallengeocr.azurewebsites.net/invoices/{nome_arquivo}'
+                            'Numero da fatura': invoice,
+                            'Data da fatura': date,
+                            'URL da fatura': f'https://rpachallengeocr.azurewebsites.net/invoices/{caminho_nome}'
                         })
         data_atual = datetime.now().strftime('%d%m%Y')
         df = pd.DataFrame(data)
-        arquivo_csv = f'dados_faturas{data_atual}.csv'
-        df.to_csv(arquivo_csv, index=False, encoding='utf-8')
+        csv_file = f'dados_faturas{data_atual}.csv'
+        df.to_csv(csv_file, index=False, encoding='utf-8')
